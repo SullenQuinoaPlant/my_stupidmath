913f592e5ef3aba77aa8c74c1ee548e75beedf14
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-08-04 01:51:47 +0200

    i

--- unit-tests/lnl.test.c
+++ unit-tests/lnl.test.c
@@ -49,7 +49,10 @@ int	declare_tests_and_run(int all_of, char *these[])
 			if (diff > max_diff)
 				max_diff = diff;
 			else if (-1 * diff > max_diff)
+			{
+				printf("yo\n");
 				max_diff = diff * -1.0L;
+			}
 		}
 printf("max_diff : %f, abs of :%f\n", max_diff, ABS(max_diff));
 printf("%f comp : %d : error : %f\n", max_diff, (max_diff < MAX_ERROR), MAX_ERROR);
