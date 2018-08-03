0b209b9f9590b2eee650a7da2bb186eeba1b9fa9
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-08-03 21:19:12 +0200

    i

--- sources/inner.h
+++ sources/inner.h
@@ -9,5 +9,9 @@ typedef struct	s_int_and_float {
 	long double	f;
 }				t_s_iaf;
 
+typedef union	u_arg_then_acc {
+	long double *arg;
+	long double	*acc;
+}				t_u_ata;
 
 #endif
--- sources/lnl.c
+++ sources/lnl.c
@@ -1,15 +1,43 @@
 # include "inner.h"
 
+/*big is used to avoid lest good approximations
+**near x=2; Not sure it's worth it considering 
+**the loss of precision.*/
 static
 t_s_iaf
 	split_x(
 		long double x)
 {
 	t_s_iaf	ret;
+	t_s_dfp	dec;
+	uint64_t	const big = (uint64_t)1 << 40;
 
-	(void)x;
-	ret = (t_s_iaf){0, 0};
+	decompose_ldouble(&x, &dec);
+	ret.i = dec.exp;
+	ret.f = make_ldouble(0, dec.mant, -LDB_MANT_PRECISION);
+	if (dec.mant > big)
+	{
+		ret.i++;
+		ret.f /=2;
+	}
 	return (ret);
+}
+
+#define SERIES_DEPTH 50
+static
+void
+	rec(
+		int rank, long double delta_pow,
+		t_u_ata delta_ret)
+{
+	if (!(rank <= SERIES_DEPTH && delta_pow))
+		*delta_ret.acc = 0;
+	else
+		rec(rank + 1, delta_pow * *delta_ret.arg, delta_ret);
+	if (rank & 1)
+		*delta_ret.acc += (delta_pow / rank);
+	erse
+		*delta_ret.acc -= (delta_pow / rank);
 }
 
 static
@@ -17,8 +45,12 @@ long double
 	series(
 		long double xx)
 {
-	(void)xx;
-	return (0);
+	int			i;
+	long double	ret;
+
+	ret = xx - 1;
+	rec(1, ret, &ret);
+	return (ret);
 }
 
 long double
