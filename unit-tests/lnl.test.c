7f907c2e1f1cd5d9ae5d50ec5a64be47ebd220de
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-08-03 21:28:46 +0200

    i

--- unit-tests/lnl.test.c
+++ unit-tests/lnl.test.c
@@ -4,10 +4,24 @@
 
 #include "mystupidmath.h"
 
+#define MAX_ERROR 0.00001
+
+#define ABS(x) \
+	((x) < 0 ? -(x) : (x))
+
 int	declare_tests_and_run(int all_of, char *these[])
 {
-	T(nop,
-		assert_true(1);
+	T(letswatch,
+		long double	x;
+		long double	diff;
+		long double	max_diff;
+
+		for (x 0.1L; x < 1; x += 0.1)
+		{
+			diff = lnl(x) - logl(x);
+			max_diff = ABS(diff) > ABS(max_diff) ? diff;
+		}
+		assert_true(max_diff < MAX_ERROR);
 	)
 	return (run_test_arr(all_of, these));
 }
