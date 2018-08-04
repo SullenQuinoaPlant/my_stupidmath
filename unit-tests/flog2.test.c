29d02378e2d03a642e9818e147afe25838830d50
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-08-04 19:01:01 +0200

    i

--- unit-tests/flog2.test.c
+++ unit-tests/flog2.test.c
@@ -7,19 +7,19 @@
 int	declare_tests_and_run(int all_of, char *these[])
 {
 	T(test0,
-		assert_true(0 == my_flog2(0));
+		assert_true(0 == flog2(0));
 	)
 	T(test1,
-		assert_true(0 == my_flog2(1));
+		assert_true(0 == flog2(1));
 	)
 	T(test2,
-		assert_true(1 == my_flog2(2));
+		assert_true(1 == flog2(2));
 	)
 	T(test3,
-		assert_true(1 == my_flog2(3));
+		assert_true(1 == flog2(3));
 	)
 	T(test4,
-		assert_true(2 == my_flog2(4));
+		assert_true(2 == flog2(4));
 	)
 	return (run_test_arr(all_of, these));
 }
