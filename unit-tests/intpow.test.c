97f766ac5702e9f2b9428b39f66bb126f39c6e49
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-08-04 03:26:02 +0200

    i

--- unit-tests/intpow.test.c
+++ unit-tests/intpow.test.c
@@ -7,22 +7,22 @@
 int	declare_tests_and_run(int all_of, char *these[])
 {
 	T(test1,
-		assert_true(4.0 == intpow(2, 2));
+		assert_true(4.0 == intpowl(2, 2));
 	)
 	T(test2,
-		assert_true(0.5L == intpow(2, -1));
+		assert_true(0.5L == intpowl(2, -1));
 	)
 	T(test3,
-		assert_true(0.0 == intpow(0, 2));
+		assert_true(0.0 == intpowl(0, 2));
 	)
 	T(test4,
-		assert_true(1.0L == intpow(2, 0));
+		assert_true(1.0L == intpowl(2, 0));
 	)
 	T(test5,
-		assert_true(4.0 == uintpow(2, 2));
+		assert_true(4.0 == uintpowl(2, 2));
 	)
 	T(test6,
-		assert_true(0.25L == intpow(0.5L, 2));
+		assert_true(0.25L == intpowl(0.5L, 2));
 	)
 
 	return (run_test_arr(all_of, these));
